63aae4d318895373da23f90fee7b84b92fc79f9f
David <davie668@gmail.com>
2020-06-29 20:43:59 +0100

    Controller setup

--- app/controllers/superheros_controller.rb
+++ app/controllers/superheros_controller.rb
@@ -1,4 +1,17 @@
 class SuperherosController < ApplicationController
   def index
+    @my_hero = Superhero.create
+    @superheros - Superhero.where.not(id: @my_hero.id)
+  end
+
+  def update
+    superhero = Superhero.find(params[:id])
+    superhero.update(superhero_params)
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :power, :weakness)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'superheros/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :superheros
+
+  root to: 'superheros/index'
 end
