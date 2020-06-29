c93c6f6757a3aa0a46caeb9c9bf66182498ed940
David <davie668@gmail.com>
2020-06-29 21:42:28 +0100

    Create new list item

--- app/javascript/channels/superheros_channel.js
+++ app/javascript/channels/superheros_channel.js
@@ -12,5 +12,25 @@ consumer.subscriptions.create("SuperherosChannel", {
   received(data) {
     // Called when there's incoming data on the websocket for this channel
     console.log(data);
+
+    const id = data.id;
+    const listItem = document.getElementById(`superhero_${id}`)
+
+    if (listItem) {
+
+    } else {
+    //  Create a new list item using the data
+      const newElement = document.createElement("li");
+      newElement.innerHTML = `
+        <li id="superhero_${data.id}">
+            <div>${data.name || ""}</div>
+            <div>${data.power || ""}</div>
+            <div>${data.weakness || ""}</div>
+        </li>
+      `
+    //  Attach new element to the list
+      const list = document.getElementById("superheros")
+      list.appendChild(newElement)
+    }
   }
 });
